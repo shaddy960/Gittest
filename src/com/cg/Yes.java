92c5984600513691b9fb93e54751baf51c2d7dab
shaddy <sonisharadace96@gmail.com>
2020-01-22 17:19:29 +0530

    Feature change

--- src/com/cg/Yes.java
+++ src/com/cg/Yes.java
@@ -4,5 +4,6 @@ public class Yes {
     public static void main(String[] args) {
         System.out.println("Yoo hoo");
         System.out.println("Hereto");
+        System.out.println("YUUO");
     }
 }
